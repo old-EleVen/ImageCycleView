944c4b261b3334c6019074ee875f630bf9c55915
sy <610034663@qq.com>
2016-05-28 16:29:30 +0800

    add module

---
+++ app/src/main/java/com/example/sy/mycycleview/module.java
@@ -0,0 +1,25 @@
+package com.example.sy.mycycleview;
+
+/**
+ * Created by sy on 2016/5/28.
+ */
+public class module {
+    String a;
+    int b;
+
+    public String getA() {
+        return a;
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+}
